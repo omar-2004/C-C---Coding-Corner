09e9d71c6f2c70f0d650fe00c90104fb249b4319
omar-2004 <touil.omar44@gmail.com>
2023-11-04 02:55:50 +0100

    Day 01

--- Introduction to C Programming/main.c
+++ Introduction to C Programming/main.c
@@ -5,6 +5,6 @@ int main(void)
     int age;
     printf("Enter your age: ");
     scanf("%d", &age);
-    printf("Here is your age: %d", age);
+    printf("Here is your birth year: %d", 2023 - age);
     return 0;
 }
